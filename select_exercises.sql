949f30c078e2eb42bff381a0e46fcda543c5fde9
Ryan Harris <ryan.t.harris67@gmail.com>
2022-10-25 14:01:41 -0500

    SELECT File updated

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,11 @@
 USE codeup_test_db;
 
+SELECT record_name FROM albums WHERE artist_name = 'Pink Floyd';
+
+SELECT release_year FROM albums WHERE record_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT release_year FROM albums WHERE release_year >= 1990;
+
+SELECT record_name FROM albums WHERE record_sales <= 20;
+
+SELECT record_name FROM albums WHERE record_genre = 'Rock';
