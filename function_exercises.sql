ae9e07191dde0f3bad216315609e4887ae502255
Ryan Harris <ryan.t.harris67@gmail.com>
2022-10-26 16:37:47 -0500

    FUNCTIONS Exercise Updated

--- function_exercises.sql
+++ function_exercises.sql
@@ -6,6 +6,6 @@ SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
 
 SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25;
 
-SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC;
 
 SELECT datediff(curdate(), hire_date) FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date desc;
