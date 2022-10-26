ed8a5d636c39642940b157dfe39aa241b4a3f498
Ryan Harris <ryan.t.harris67@gmail.com>
2022-10-26 16:02:23 -0500

    FUNCTION Exercise Complete

---
+++ function_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+
+SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25;
+
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
+
+SELECT datediff(curdate(), hire_date) FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date desc;
