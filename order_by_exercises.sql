af629571798d847dc5b776be947584ee7a6bb617
Ryan Harris <ryan.t.harris67@gmail.com>
2022-10-26 14:29:26 -0500

    ORDER BY Exercise complete

---
+++ order_by_exercises.sql
@@ -0,0 +1,26 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name ='Vidya' OR first_name = 'Maya';
+
+SELECT * FROM employees WHERE gender = 'M' AND first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name;
+
+SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no;
+
+
+
